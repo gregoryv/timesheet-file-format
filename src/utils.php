b064416e012f4beba7514e5a07e1056b42dab874
Gregory Vincic <g@7de.se>
2015-10-04 08:40:12 +0200

    Documenting utils.php

--- src/utils.php
+++ src/utils.php
@@ -1,6 +1,12 @@
 <?php
+namespace gregoryv\timesheet;
+
 /**
  * Sets default value of the key in the given array if not already set.
+ *
+ * @param array $arr Reference to array
+ * @param string $key Key in array to set
+ * @param mixed $value The default value to set
  */
 function setDefault(&$arr, $key, $value)
 {
@@ -9,6 +15,10 @@ function setDefault(&$arr, $key, $value)
 
 /**
  * Adds the value to the keyed value, default is 0.
+ *
+ * @param array $arr Reference to array
+ * @param string $key Key in array to set
+ * @param number $value The value to add to an existing one
  */
 function addTo(&$arr, $key, $value) {
   setDefault($arr, $key, 0);
