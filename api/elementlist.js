f6c3b4d67db7e0c8496e4a897c321bb9a7181369
Gregory Vincic <g@7de.se>
2016-05-04 06:29:13 +0200

    Time is now TimeTag

--- api/elementlist.js
+++ api/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","ArrayAccess"],["c","ArrayObject"],["c","Countable"],["c","gregoryv\\timesheet\\Sheet"],["c","gregoryv\\timesheet\\SheetGenerator"],["c","gregoryv\\timesheet\\Time"],["c","gregoryv\\timesheet\\Totals"],["c","IteratorAggregate"],["c","Serializable"],["c","Traversable"]];
+ApiGen.elements = [["c","ArrayAccess"],["c","ArrayObject"],["c","Countable"],["c","gregoryv\\timesheet\\Sheet"],["c","gregoryv\\timesheet\\SheetGenerator"],["c","gregoryv\\timesheet\\TimeTag"],["c","gregoryv\\timesheet\\Totals"],["c","IteratorAggregate"],["c","Serializable"],["c","Traversable"]];
